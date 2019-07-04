4c4a908c93fb67ba74d4d9854d786b7f38f253bd
SwarnalathaNagaraj <52522997+SwarnalathaNagaraj@users.noreply.github.com>
2019-07-04 11:57:37 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.io.*;
+class Main{
+ public static void main(String args[])throws IOException{
+ BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+ String s=br.readLine();
+ if(s.equals("swarna")
+  System.out.println("True");
+ else
+  System.out.println("False");
+ }
+}
